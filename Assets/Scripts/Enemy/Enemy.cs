3e81ef4c7de52dc86ee0484f9e0be9764c4a1457
shram78 <ra00275@gmail.com>
2021-10-20 00:02:48 +0300

    complete

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private AudioSource _destroySound;
+
+    private Animator _animator;
+    private const string _isDestroy = "isDestroy";
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            player.AddScore();
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _animator.SetBool(_isDestroy, true);
+        _destroySound.Play();
+        Destroy(gameObject, 0.5f);
+    }
+}
---
+++ Assets/Scripts/Player/Impact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impact : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _particleSystem;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            _particleSystem.Play();
+    }
+}
